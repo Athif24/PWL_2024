8e647e946c75fae4032a9d6c0c43f2d2bd234475
Athif24 <athifattaqy24@gmail.com>
2024-09-04 09:09:43 +0700

    Praktikum 1 JS2 (Basic Routing)

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,49 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/hello', function() {
+    return 'Hello World';
+});
+
+Route::get('/world', function() {
+    return 'World';
+});
+
+Route::get('/pesan', function() {
+    return 'Selamat Datang';
+});
+
+Route::get('/about', function() {
+    return '2241760107';
+});
+
+//------------------------------------------------------------------------------------------------
+
+Route::get('/user/{name}', function($name) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel '.$id;
+});
+
+//------------------------------------------------------------------------------------------------
+
+Route::get('/user/{name?}', function ($name=null) {
+    return 'Nama saya '.$name;
+});
+
+Route::get('/user/{name?}', function ($name='John') {
+    return 'Nama saya '.$name;
+});
+// Route::get('/user/{name?}', function ($name = null) {
+//     return 'Nama saya '.$name;
+// });
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
